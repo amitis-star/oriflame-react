bb7b1b6e2b9d8f0cc0eb4a3ec9916a7eef75bf74
= <you@example.com>
2025-07-07 08:56:13 +0330

    nutrition response 1024

--- src/components/Header/MegaMenu/RightPanel/Nutrition/index.tsx
+++ src/components/Header/MegaMenu/RightPanel/Nutrition/index.tsx
@@ -13,28 +13,41 @@ const NutritionItems = [
 ];
 
 const NutritionPanel: React.FC = () => (
-  <div className="min-h-full p-6 h-[885px] w-[900px] bg-white">
+  <div
+    className="
+      p-6 bg-white
+      lg:w-[614px]       
+      xl:w-[900px]      
+    "
+  >
     <div className="p-7.5 flex items-center cursor-pointer">
       <p className="text-20 font-700 text-gray-700">All Nutrition</p>
       <RightArrow className="ml-2.5 text-gray-700 w-7.5 h-7.5" />
     </div>
 
-    <div className="pt-2.5 px-7.5 pb-20 flex flex-wrap">
+    <div
+      className="
+        pt-2.5 px-7.5 pb-20
+        flex flex-wrap
+        gap-x-7.5         
+        gap-y-4           
+      "
+    >
       {NutritionItems.map((item, idx) => {
         const isLast = idx === NutritionItems.length - 1;
         return (
           <div key={idx}>
             <p
-              className={`text-gray-700 font-600 cursor-pointer hover:text-gray-400 ${
-                isLast ? "pl-1 pb-5" : "pl-1 pr-7.5 pb-7.5"
-              }`}
+              className={`
+                text-gray-700 font-600 cursor-pointer hover:text-gray-400
+                ${isLast ? "pb-5" : "pb-7.5"}
+              `}
             >
               {item}
             </p>
+
             {isLast && (
-              <p className="text-14 text-gray-500 font-500 -mt-1 pl-1">
-                Wellosophy
-              </p>
+              <p className="text-14 text-gray-500 font-500 -mt-1">Wellosophy</p>
             )}
           </div>
         );
